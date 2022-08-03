0d73b9b2b0dbd4863f336e356504bbe20553e68a
Simon Parry <simon.parry@karoshibee.com>
2022-08-03 10:50:10 +0100

    server now acts as dap server

--- server.ml
+++ server.ml
@@ -7,85 +7,94 @@
 module DRq = Dapper.Dap_request
 module DRs = Dapper.Dap_response
 
-(* Shared mutable counter *)
-let counter = ref 0
 
 let backlog = 10
 
 type event =
   | NextReq of DRq.NextRequest.cls_t
+  | StackTrace of DRq.StackTraceRequest.cls_t
   | Unknown of string
 
 
 let handle_message msg =
   let open Data_encoding.Json in
-  let parse_req ~err ~event f js =
+  let parse_next_req js =
     try
-      let req = f js in
-      Some (event req)
+      let req = destruct DRq.NextRequest.enc js in
+      Some (NextReq req)
     with _ ->
-      let _ = Logs_lwt.warn (fun m -> m err) in
+      let _ = Logs_lwt.warn (fun m -> m "Not next request") in
       None
   in
-  match from_string msg with
-  | Ok js ->
-    let next_req = parse_req
-        ~err:"Not NextReq"
-        ~event:(fun req -> NextReq req)
-        (destruct DRq.NextRequest.enc)
-        js
-    in
-      Option.value ~default:(Unknown "TODO") next_req
-
-  | Error err ->
-    let s = Printf.sprintf "Unknown command '%s' - error '%s'" msg err in
-    Unknown s
-
+  let parse_stacktrace_req js =
+    try
+      let req = destruct DRq.StackTraceRequest.enc js in
+      Some (StackTrace req)
+    with _ ->
+      let _ = Logs_lwt.warn (fun m -> m "Not stack trace request") in
+      None
+  in
+  match msg with
+  | "n" -> (
+      let args = DRq.NextArguments.{threadId=1L; singleThread=None; granularity=None} in
+      let req = new DRq.NextRequest.cls 1L args in
+      NextReq req
+    )
+  | "st" -> (
+      let args = DRq.StackTraceArguments.{threadId=1L; startFrame=None; levels=None} in
+      let req = new DRq.StackTraceRequest.cls 1L args in
+      StackTrace req
+    )
+  | _ -> (
+      match from_string msg with
+      | Ok js -> (
+          match parse_next_req js with
+          | Some next_req -> next_req
+          | None -> match parse_stacktrace_req js with
+            | Some st_req -> st_req
+            | None -> Unknown "Unknown js"
+        )
+      | Error err ->
+        let s = Printf.sprintf "Unknown command '%s' - error '%s'" msg err in
+        Unknown s
+    )
 
 let rec handle_connection ic oc ic_process oc_process () =
+  let open Data_encoding.Json in
   Lwt_io.read_line_opt ic >>=
   (fun msg ->
      match msg with
      | Some msg -> (
          let next =
            match handle_message msg with
-           | _ -> Logs_lwt.warn (fun m -> m "TODO")
-           (* | Dap js -> ( *)
-           (*     (\* if its a next request - step forward *\) *)
-           (*     let open Data_encoding.Json in *)
-           (*     try *)
-           (*       let req = destruct DRq.NextRequest.enc js in *)
-           (*       Printf.(fprintf oc "%s\n" resp; flush oc); *)
-           (*     with _ *)
-           (*       Logs_lwt.warn (fun m -> m "Not NextRequest") *)
+           | NextReq req -> (
+             let seq = Int64.succ req#seq in
+             let request_seq = req#seq in
+             let success = true in
+             let command = req#command in
+             let resp = new DRs.NextResponse.cls seq request_seq success command in
+             let resp = construct DRs.NextResponse.enc resp |> to_string |> Defaults._replace "\n" "" in
+             try
+               Printf.fprintf oc_process "step\n"; flush oc_process; Logs_lwt.info (fun m -> m "Next response \n%s\n" resp);
+             with Sys_error _ ->
+               (* run out of contract to step through *)
+               try
+                 let _ = Unix.close_process (ic_process, oc_process) in (); Logs_lwt.warn (fun m -> m "Process finished")
+               with Unix.Unix_error _ ->
+                 Logs_lwt.warn (fun m -> m "Process finished")
+           )
+           | StackTrace req ->
+             let seq = Int64.succ req#seq in
+             let request_seq = req#seq in
+             let success = true in
+             let command = req#command in
+             (* TODO read frames from ic_process *)
+             let body = DRs.StackTraceResponse.{stackFrames=[]; totalFrames=None} in
+             let resp = new DRs.StackTraceResponse.cls seq request_seq success command body in
+             let resp = construct DRs.StackTraceResponse.enc resp |> to_string |> Defaults._replace "\n" "" in
+             Logs_lwt.info (fun m -> m "Stack trace response \n%s\n" resp);
+           | Unknown s -> Logs_lwt.warn (fun m -> m "Unknown '%s'" s)
 
-           (*     let seq = Int64.succ req#seq in *)
-           (*     let request_seq = req#seq in *)
-           (*     let success = true in *)
-           (*     let command = req#command in *)
-           (*     let resp = new DRs.NextResponse.cls seq request_seq success command in *)
-           (*     let resp = construct DRs.NextResponse.enc resp |> to_string |> Defaults._replace "\n" "" in *)
-           (*     Printf.(fprintf oc "%s\n" resp; flush oc); *)
-
-           (*   ) *)
-           (* | Step n -> ( *)
-           (*   (\* WANT TO WRITE TO PROCESS STDIN/OUT  *\) *)
-           (*   let open Dapper.Dap_request in *)
-           (*   let args = NextArguments.{threadId=1L; singleThread=None; granularity=None} in *)
-           (*   let rq = new NextRequest.cls (Int64.of_int n) args in *)
-           (*   let request = Data_encoding.Json.(construct NextRequest.enc rq |> to_string |> Defaults._replace "\n" "" ) in *)
-           (*   try *)
-           (*     Printf.fprintf oc_process "%s\n" request; flush oc_process; Logs_lwt.info (fun m -> m "Stepping with \n%s\n" request) *)
-           (*   with Sys_error _ -> *)
-           (*     (\* run out of contract to step through *\) *)
-           (*     try *)
-           (*       let _ = Unix.close_process (ic_process, oc_process) in (); Logs_lwt.warn (fun m -> m "Process finished") *)
-           (*     with Unix.Unix_error _ -> *)
-           (*       Logs_lwt.warn (fun m -> m "Process finished") *)
-           (* ) *)
-
-           (* | Counter s | Unknown s -> *)
-           (*   Lwt_io.write_line oc s *)
          in
          next >>= handle_connection ic oc ic_process oc_process
      )
@@ -98,7 +107,7 @@ let accept_connection cmd conn =
   let ic_process, oc_process = Unix.open_process cmd in
   let jobs = Lwt.join [handle_connection ic oc ic_process oc_process ()] in
   Lwt.on_failure (jobs >>= return) (fun e -> Logs.err (fun m -> m "%s" (Printexc.to_string e) ));
-  Logs_lwt.info (fun m -> m "New connection") >>= Lwt.return
+  Logs_lwt.info (fun m -> m "New connection\n") >>= Lwt.return
 
 let create_socket listen_address port () =
   let sock = Lwt_unix.(socket PF_INET SOCK_STREAM 0) in
