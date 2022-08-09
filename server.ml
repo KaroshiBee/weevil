71f9251c82024810d5cb5e0c562bb4e1566d71ea
Simon Parry <simon.parry@karoshibee.com>
2022-08-09 11:32:49 +0100

    added handler for scopes request
    also tidied up handling of parsers

--- server.ml
+++ server.ml
@@ -4,6 +4,7 @@
 
     Build with: ocamlfind ocamlopt -package lwt,lwt.unix,logs,logs.lwt -linkpkg -o counter-server ./counter-server.ml
  *)
+open Data_encoding.Json
 module DRq = Dapper.Dap_request
 module DRs = Dapper.Dap_response
 module DEv = Dapper.Dap_event
@@ -14,6 +15,7 @@ let backlog = 10
 type event =
   | NextReq of DRq.NextRequest.cls_t
   | StackTrace of DRq.StackTraceRequest.cls_t
+  | Scopes of DRq.ScopesRequest.cls_t
   | Unknown of string
 
 (* TODO also want to do scopes and variables
@@ -24,24 +26,37 @@ type event =
  *  and there are multiple variables which are indexed
  *  children of the main michelson stack *)
 
+let parse_next_req js =
+  try
+    let req = destruct DRq.NextRequest.enc js in
+    Some (NextReq req)
+  with _ ->
+    let _ = Logs_lwt.warn (fun m -> m "Not next request") in
+    None
+
+let parse_stacktrace_req js =
+  try
+    let req = destruct DRq.StackTraceRequest.enc js in
+    Some (StackTrace req)
+  with _ ->
+    let _ = Logs_lwt.warn (fun m -> m "Not stack trace request") in
+    None
+
+let parse_scopes_req js =
+  try
+    let req = destruct DRq.ScopesRequest.enc js in
+    Some (Scopes req)
+  with _ ->
+    let _ = Logs_lwt.warn (fun m -> m "Not scopes request") in
+    None
+
+let parsers = [
+  parse_next_req;
+  parse_stacktrace_req;
+  parse_scopes_req;
+]
+
 let handle_message msg =
-  let open Data_encoding.Json in
-  let parse_next_req js =
-    try
-      let req = destruct DRq.NextRequest.enc js in
-      Some (NextReq req)
-    with _ ->
-      let _ = Logs_lwt.warn (fun m -> m "Not next request") in
-      None
-  in
-  let parse_stacktrace_req js =
-    try
-      let req = destruct DRq.StackTraceRequest.enc js in
-      Some (StackTrace req)
-    with _ ->
-      let _ = Logs_lwt.warn (fun m -> m "Not stack trace request") in
-      None
-  in
   match msg with
   (* NOTE 'n' and 'st' are helpers to quickly test stuff *)
   | "n" -> (
@@ -54,17 +69,21 @@ let handle_message msg =
       let req = new DRq.StackTraceRequest.cls 1L args in
       StackTrace req
     )
+  | "sc" -> (
+      let args = DRq.ScopesArguments.{frameId=Defaults._THE_FRAME_ID} in
+      let req = new DRq.ScopesRequest.cls 1L args in
+      Scopes req
+    )
   | _ -> (
       (* NOTE when not using helpers,
          need to strip off Content-Length header field
          and get inner object *)
       match Result.bind (Defaults.strip_header msg) from_string with
       | Ok js -> (
-          match parse_next_req js with
-          | Some next_req -> next_req
-          | None -> match parse_stacktrace_req js with
-            | Some st_req -> st_req
-            | None -> Unknown "Unknown js"
+          (* NOTE just grab the first event type that can be parsed *)
+          match List.filter_map (fun p -> p js) parsers with
+          | req :: _ -> req
+          | [] -> Unknown (Printf.sprintf "Unknown js: %s" msg)
         )
       | Error err ->
         let s = Printf.sprintf "Unknown command '%s' - error '%s'" msg err in
@@ -100,7 +119,7 @@ let read_weevil_log () =
     )
   |> List.map (fun wrec ->
       let loc = Model.Weevil_json.(wrec.location) in
-      Db.StackFrame.{id=1L; name="unknown"; line=loc; column=0L}
+      Db.StackFrame.{id=Defaults._THE_FRAME_ID; name=Defaults._THE_FRAME_NAME; line=loc; column=0L}
     )
 
 let rec handle_connection ic oc ic_process oc_process () =
@@ -112,49 +131,67 @@ let rec handle_connection ic oc ic_process oc_process () =
          let next =
            match handle_message msg with
            | NextReq req -> (
-             let seq = Int64.succ req#seq in
-             let request_seq = req#seq in
-             let success = ref true in
-             let command = req#command in
-             let _ =
-               try
-                 Printf.fprintf oc_process "step\n"; flush oc_process; Logs_lwt.info (fun m -> m "stepping")
-               with Sys_error _ -> (
-                   success := false;
-                   (* run out of contract to step through *)
-                   try
-                     let _ = Unix.close_process (ic_process, oc_process) in (); Logs_lwt.warn (fun m -> m "Process finished: sys error")
-                   with Unix.Unix_error _ ->
-                     Logs_lwt.warn (fun m -> m "Process finished: unix error")
-                 )
-             in
-             (* send the response *)
-             let resp = new DRs.NextResponse.cls seq request_seq !success command in
-             let resp = construct DRs.NextResponse.enc resp |> Defaults.wrap_header in
-             (* send stopped event too *)
-             let seq = Int64.succ seq in
-             let body = DEv.StoppedEvent.(body ~reason:Step ()) in
-             let ev = new DEv.StoppedEvent.cls seq body in
-             let ev = construct DEv.StoppedEvent.enc ev |> Defaults.wrap_header in
-             Logs_lwt.info (fun m -> m "Next response \n%s\nStopped event\n%s\n" resp ev);
-           )
+               let seq = Int64.succ req#seq in
+               let request_seq = req#seq in
+               let success = ref true in
+               let command = req#command in
+               let _ =
+                 try
+                   Printf.fprintf oc_process "step\n"; flush oc_process; Logs_lwt.info (fun m -> m "stepping")
+                 with Sys_error _ -> (
+                     success := false;
+                     (* run out of contract to step through *)
+                     try
+                       let _ = Unix.close_process (ic_process, oc_process) in (); Logs_lwt.warn (fun m -> m "Process finished: sys error")
+                     with Unix.Unix_error _ ->
+                       Logs_lwt.warn (fun m -> m "Process finished: unix error")
+                   )
+               in
+               (* send the response *)
+               let resp = new DRs.NextResponse.cls seq request_seq !success command in
+               let resp = construct DRs.NextResponse.enc resp |> Defaults.wrap_header in
+               (* send stopped event too *)
+               let seq = Int64.succ seq in
+               let body = DEv.StoppedEvent.(body ~reason:Step ()) in
+               let ev = new DEv.StoppedEvent.cls seq body in
+               let ev = construct DEv.StoppedEvent.enc ev |> Defaults.wrap_header in
+               Logs_lwt.info (fun m -> m "Next response \n%s\nStopped event\n%s\n" resp ev);
+             )
            | StackTrace req -> (
-             let seq = Int64.succ req#seq in
-             let request_seq = req#seq in
-             let success = true in
-             let command = req#command in
-             (* TODO should be able to arrange so that can read frames from ic_process? *)
-             let stackFrames =
-               match read_weevil_log () |> List.rev |> List.hd with
-               | None -> []
-               | Some last_ln -> [last_ln]
-             in
-             let totalFrames = Some (List.length stackFrames |> Int64.of_int) in
-             let body = DRs.StackTraceResponse.{stackFrames; totalFrames} in
-             let resp = new DRs.StackTraceResponse.cls seq request_seq success command body in
-             let resp = construct DRs.StackTraceResponse.enc resp |> Defaults.wrap_header in
-             Logs_lwt.info (fun m -> m "Stack trace response \n%s\n" resp);
-           )
+               let seq = Int64.succ req#seq in
+               let request_seq = req#seq in
+               let success = true in
+               let command = req#command in
+               (* TODO should be able to arrange so that can read frames from ic_process? *)
+               let stackFrames =
+                 match read_weevil_log () |> List.rev |> List.hd with
+                 | None -> []
+                 | Some last_ln -> [last_ln]
+               in
+               let totalFrames = Some (List.length stackFrames |> Int64.of_int) in
+               let body = DRs.StackTraceResponse.{stackFrames; totalFrames} in
+               let resp = new DRs.StackTraceResponse.cls seq request_seq success command body in
+               let resp = construct DRs.StackTraceResponse.enc resp |> Defaults.wrap_header in
+               Logs_lwt.info (fun m -> m "Stack trace response \n%s\n" resp);
+             )
+           | Scopes req -> (
+               (* TODO would need to pull the frameId from req and look up the scopes from that *)
+               let seq = Int64.succ req#seq in
+               let request_seq = req#seq in
+               let success = true in
+               let command = req#command in
+               let locals_name, locals_var = Defaults._THE_ONLY_SCOPE in
+               (* let gas_name, gas_var = Defaults._THE_GAS_LOCAL in
+                * let stack_name, stack_var = Defaults._THE_MICHELSON_STACK_LOCAL in *)
+               let scopes = [
+                 Db.Scope.{name=locals_name; variablesReference=locals_var; expensive=false};
+               ]
+               in
+               let body = DRs.ScopesResponse.{scopes} in
+               let resp = new DRs.ScopesResponse.cls seq request_seq success command body in
+               let resp = construct DRs.ScopesResponse.enc resp |> Defaults.wrap_header in
+               Logs_lwt.info (fun m -> m "Scopes response \n%s\n" resp);
+             )
            | Unknown s -> Logs_lwt.warn (fun m -> m "Unknown '%s'" s)
 
          in
