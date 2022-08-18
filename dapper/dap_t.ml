29759cf5553df2478dac00b8730832c9b26f30b6
Simon Parry <simon.parry@karoshibee.com>
2022-08-18 13:18:08 +0100

    use functors to make each Request/Response/Event
    each message type in the spec is either a
    'leaf' (enum, plain object, array, empty object)
    or it is one of Request/Response/Event type

--- dapper/dap_t.ml
+++ dapper/dap_t.ml
@@ -1,235 +1,132 @@
-open Data_encoding
+(* there are three message types: Request, Response, Event
+   they all three have to conform to the ProtocolMessage spec
+   which is having fields "seq" & "type"
+   all leaf types are auto-generated (enums, plain objects etc)
+   and written to Dap_enum.ml and then used in the following functors
 
-module ProtocolMessage = struct
+   NOTE According to the spec
+   - The arguments field of Requests can sometimes be optional,
+      so we have it required always and use an empty object when
+      it is not needed
+   - The body field of Event and Response can sometimes be optional
+      so we have it required always and use an empty object when
+      it is not needed
+   - The message field of Response is supposed to be an _enum
+      but the spec only specifies "cancelled" as a suggestion
+      and no-where is it used, so we keep it as a string option
+*)
 
-  type msg =
-    | Request
-    | Response
-    | Event
+module E = Dap_enum
 
-  type t = {
-    seq: int64;
-    type_: msg;
-  }
-
-  let msg_e =
-    conv
-      ( function | Request -> "request" | Response -> "response" | Event -> "event" )
-      ( fun msg -> match msg with | "request" -> Request | "response" -> Response | "event" -> Event | _ -> failwith "ERROR: unkown msg type" )
-      string
-
-  let e =
-    let description = "Sequence number (also known as message ID). For protocol messages of type 'request' this ID can be used to cancel the request." in
-    conv
-      (fun {seq; type_} -> (seq, type_))
-      (fun (seq, type_) -> {seq; type_})
-      (obj2
-         (req ~description "seq" int64)
-         (req ~description:"Message type." "type" msg_e))
+module type ENC0 = sig
+  type t
+  val enc : t Data_encoding.encoding
 end
 
-module Request = struct
+module type CMD = sig
+  val cmd : E.Command.t
+end
 
-  type 'args t = {
-    seq: int64;
-    type_: ProtocolMessage.msg;
-    command: string;
-    arguments: 'args option;
+
+module type REQUEST = sig
+  type args
+  type t
+  val seq : t -> int64
+  val type_ : t -> E.ProtocolMessage_type.t
+  val command : t -> E.Command.t
+  val arguments : t -> args
+
+  val enc : t Data_encoding.encoding
+  val make : seq:int64 -> arguments:args -> unit -> t
+
+
+end
+
+
+module MakeRequest (C:CMD) (ARGS:ENC0) : (REQUEST with type args := ARGS.t) = struct
+  type t = {
+    seq : int64;
+    type_ : E.ProtocolMessage_type.t;
+    command : E.Command.t;
+    arguments : ARGS.t;
   }
 
-  let msg_e =
-    let open ProtocolMessage in
-    conv
-      ( function | Request -> "request" | _ -> failwith "ERROR: expected Request")
-      ( fun msg -> match msg with | "request" -> Request | _ -> failwith "ERROR: expected 'request'" )
-      string
+  let seq t = t.seq
+  let type_ t = t.type_
+  let command t = t.command
+  let arguments t = t.arguments
 
-  let e args =
+
+  let enc =
+    let open Data_encoding in
     conv
       (fun {seq; type_; command; arguments} -> (seq, type_, command, arguments))
       (fun (seq, type_, command, arguments) -> {seq; type_; command; arguments})
       (obj4
          (req "seq" int64)
-         (req "type" msg_e)
-         (req "command" string)
-         (opt "arguments" args))
+         (req "type" E.ProtocolMessage_type.enc)
+         (req "command" E.Command.enc)
+         (req "arguments" ARGS.enc))
+
+  let make ~seq ~arguments () =
+    let type_ = E.ProtocolMessage_type.Request in
+    let command = C.cmd in
+    {seq; type_; command; arguments}
 
 end
 
 module Event = struct
-
   type 'body t = {
-    seq: int64;
-    type_: ProtocolMessage.msg;
-    event: string;
-    body: 'body option;
+    seq : int64;
+    type_ : E.ProtocolMessage_type.t;
+    event : E.Event.t;
+    body : 'body;
   }
 
-  let msg_e =
-    let open ProtocolMessage in
-    conv
-      ( function | Event -> "event" | _ -> failwith "ERROR: expected Event")
-      ( fun msg -> match msg with | "event" -> Event | _ -> failwith "ERROR: expected 'event'" )
-      string
-
-  let e body =
+  let enc body =
+    let open Data_encoding in
     conv
       (fun {seq; type_; event; body} -> (seq, type_, event, body))
       (fun (seq, type_, event, body) -> {seq; type_; event; body})
       (obj4
          (req "seq" int64)
-         (req "type" msg_e)
-         (req "event" string)
-         (opt "body" body))
+         (req "type" E.ProtocolMessage_type.enc)
+         (req "event" E.Event.enc)
+         (req "body" body))
+
+  let make ~seq ~type_ ~event ~body () =
+    {seq; type_; event; body}
 
 end
 
 module Response = struct
-
   type 'body t = {
-    seq: int64;
-    type_: ProtocolMessage.msg;
-    request_seq: int64;
-    success: bool;
-    command: string;
-    message: string option;
-    body: 'body option;
+    seq : int64;
+    type_ : E.ProtocolMessage_type.t;
+    request_seq : int64;
+    success : bool;
+    command : E.Command.t;
+    message : string option; (* only used once I think, keep as string for now *)
+    body : 'body;
   }
 
-  let msg_e =
-    let open ProtocolMessage in
+  let enc body =
+    let open Data_encoding in
     conv
-      ( function | Response -> "response" | _ -> failwith "ERROR: expected Response")
-      ( fun msg -> match msg with | "response" -> Response | _ -> failwith "ERROR: expected 'response'" )
-      string
+      (fun {seq; type_; request_seq; success; command; message; body} ->
+        (seq, type_, request_seq, success, command, message, body))
+      (fun (seq, type_, request_seq, success, command, message, body) ->
+        {seq; type_; request_seq; success; command; message; body})
+      (obj7
+         (req "seq" int64)
+         (req "type" E.ProtocolMessage_type.enc)
+         (req "request_seq" int64)
+         (req "success" bool)
+         (req "command" E.Command.enc)
+         (opt "message" string)
+         (req "body" body))
 
-  let response_e body =
-    obj7
-      (req "seq" int64)
-      (req "type" msg_e)
-      (req "request_seq" int64)
-      (req "success" bool)
-      (req "command" string)
-      (opt "message" string)
-      (opt "body" body)
+  let make ~seq ~type_ ~request_seq ~success ~command ?message ~body () =
+    {seq; type_; request_seq; success; command; message; body}
 
-  let e body =
-    conv
-      (fun {seq; type_; request_seq; success; command; message; body} -> (seq, type_, request_seq, success, command, message, body))
-      (fun (seq, type_, request_seq, success, command, message, body) -> {seq; type_; request_seq; success; command; message; body})
-      (response_e body)
-end
-
-module Message = struct
-  type t = {
-    id: int64;
-    format: string;
-    variables: (string * string) list option;
-    sendTelemetry: bool option;
-    showUser: bool option;
-    url: string option;
-    urlLabel: string option;
-  }
-
-  let msg_e =
-    obj7
-      (req "id" int64)
-      (req "format" string)
-      (opt "variables" (list @@ (tup2 string string)))
-      (opt "sendTelemetry" bool)
-      (opt "showUser" bool)
-      (opt "url" string)
-      (opt "urlLabel" string)
-
-  let e =
-    conv
-      (fun {id; format; variables; sendTelemetry; showUser; url; urlLabel} -> (id, format, variables, sendTelemetry, showUser, url, urlLabel))
-      (fun (id, format, variables, sendTelemetry, showUser, url, urlLabel) -> {id; format; variables; sendTelemetry; showUser; url; urlLabel})
-      msg_e
-end
-
-module Error = struct
-    type t = {
-      error: Message.t option;
-    }
-
-    let e =
-      conv
-        (fun {error} -> error)
-        (fun error -> {error})
-        (obj1 (opt "error" Message.e))
-
-  end
-
-
-module ErrorResponse = struct
-
-  type t = {
-    seq: int64;
-    type_: ProtocolMessage.msg;
-    request_seq: int64;
-    success: bool;
-    command: string;
-    message: string option;
-    body: Error.t;
-  }
-
-  let msg_e =
-    let open ProtocolMessage in
-    conv
-      ( function | Response -> "response" | _ -> failwith "ERROR: expected Response")
-      ( fun msg -> match msg with | "response" -> Response | _ -> failwith "ERROR: expected 'response'" )
-      string
-
-  let response_e =
-    obj7
-      (req "seq" int64)
-      (req "type" msg_e)
-      (req "request_seq" int64)
-      (req "success" bool)
-      (req "command" string)
-      (opt "message" string)
-      (req "body" Error.e)
-
-  let e =
-    conv
-      (fun {seq; type_; request_seq; success; command; message; body} -> (seq, type_, request_seq, success, command, message, body))
-      (fun (seq, type_, request_seq, success, command, message, body) -> {seq; type_; request_seq; success; command; message; body})
-      response_e
-end
-
-module CancelArguments = struct
-  type t = {
-    requestId: int64 option;
-    progressId: string option;
-  }
-
-  let e =
-    conv
-      (fun {requestId; progressId} -> (requestId, progressId))
-      (fun (requestId, progressId) -> {requestId; progressId})
-      (obj2
-         (opt "requestId" int64)
-         (opt "progressId" string))
-
-  let e_ =
-    obj2
-      (opt "requestId" int64)
-      (opt "progressId" string)
-end
-
-module CancelRequest = struct
-
-  let cmd_e =
-    obj2
-      (req "command" (constant "cancel"))
-      (opt "arguments" CancelArguments.e)
-
-  let e =
-    merge_objs (Request.e CancelArguments.e) cmd_e
-end
-
-module CancelResponse = struct
-  let e =
-    Response.e unit
 end
