dea2b0e54e286cdda5959047c8ae972af7ec4ab7
wyn <simon.parry@karoshibee.com>
2022-10-14 14:56:35 +0100

    doing flow stuff on the data side,
    ie locking in the request/response and event pairings
    but just the data - no IO yet or interacting with debugger

--- src/dapper_gen/dap_flow.ml
+++ src/dapper_gen/dap_flow.ml
@@ -1,139 +1,271 @@
 
 module type SEQUENCED = sig
-  type 'a t
-  val seq : 'a t -> int
+  type (_, _, _) t
+  val seq : (_, _, _) t -> int
 end
 
-(* module Flow (Request:SEQUENCED) (Response:SEQUENCED) = struct *)
+module Sequencing (S:SEQUENCED) : sig
+  type t
+  val next_sequence : (_, _, _) S.t -> t
+  val seq : t -> int
+  val request_seq : t -> int
+end
+  = struct
+  type t = {seq:int; request_seq:int}
+  let next_sequence req =
+    let request_seq = S.seq req in
+    let seq = succ request_seq in
+    {seq; request_seq}
+  let seq t = t.seq
+  let request_seq t = t.request_seq
+end
 
-(*   module JS = Data_encoding.Json *)
+(* TODO error message ids need to be unique *)
+module Flow : sig
+  type launch_mode
+  type config = {launch_mode: launch_mode}
+  (* NOTE using ErrorResponse_body here because dont know the sequence number yet *)
+  type ret = (Dap_message.response * Dap_message.event list, Dap_message.ErrorResponse_body.t) Result.t
+  val request_response : config:config -> Dap_message.request -> ret
+end = struct
 
-(*   type ('request, 'response, 'event, 'error, 'cancel) t = { *)
-(*     request: 'request Request.t Data_encoding.t; *)
-(*     response: 'response Response.t Data_encoding.t; *)
-(*     events: 'event list; *)
-(*     on_error: (unit -> 'error) option; *)
-(*     on_cancel: (unit -> 'cancel) option; *)
-(*   } *)
-
-(*   let make ?on_error ?on_cancel ?(events=[]) request response = { *)
-(*       request; response; events; on_error; on_cancel *)
-(*     } *)
-
-(*   let _HEADER_FIELD = "Content-Length: " *)
-(*   let _HEADER_TOKEN = "\r\n\r\n" *)
-
-(*   let _replace input output = *)
-(*     Str.global_replace (Str.regexp_string input) output *)
-
-(*   let wrap_header js = *)
-(*     let s = js *)
-(*             |> JS.to_string *)
-(*             |> _replace "\n" "" *)
-(*     in *)
-(*     let n = String.length s in *)
-(*     Printf.sprintf "%s%d%s%s" _HEADER_FIELD n _HEADER_TOKEN s *)
-
-(*   let destruct_request t msg = *)
-(*       match JS.from_string msg with *)
-(*       | Ok js -> ( *)
-(*           try *)
-(*             Ok (JS.destruct t.request js) *)
-(*           with _ as err -> *)
-(*             Logs.err (fun m -> m "Cannot parse json '%s' as request: '%s'" msg @@ Printexc.to_string err); *)
-(*             Error (Printexc.to_string err) *)
-(*         ) *)
-(*       | Error err -> *)
-(*         Logs.err (fun m -> m "Cannot parse json '%s': '%s'" msg err); *)
-(*         (\* TODO should return an error response *\) *)
-(*         Error err *)
-
-
-(*   let construct_response t response = *)
-(*     let r = Response.incr response in *)
-(*     JS.construct t.response r |> wrap_header *)
-
-
-(* end *)
-(* open Dap_encoders *)
-
-(* module Sequencing (S:SEQUENCED) : sig *)
-(*   type t = {seq:int; request_seq:int} *)
-(*   val correct_sequence : 'a S.t -> t *)
-(* end *)
-(*   = struct *)
-(*   type t = {seq:int; request_seq:int} *)
-(*   let correct_sequence req = *)
-(*     let request_seq = S.seq req in *)
-(*     let seq = succ request_seq in *)
-(*     {seq; request_seq} *)
-(* end *)
-
-(* module type RequestResponse = sig *)
-(*   type body *)
-(*   type 'body t *)
-(*   val of_request : request -> (body -> response, string) Result.t *)
-(* end *)
-
-(* module InitFlow : (RequestResponse with type body := Capabilities.t and type _ t = Capabilities.t InitializeResponseMessage.t) = struct *)
-(*   type _ t = Capabilities.t InitializeResponseMessage.t *)
-(*   let of_request = function *)
-(*     | InitializeRequest req -> *)
-(*       let module S = Sequencing (InitializeRequestMessage) in *)
-(*       let {S.seq; request_seq} = S.correct_sequence req in *)
-(*       let resp = fun body -> InitializeResponse (InitializeResponseMessage.make ~seq ~request_seq ~success:true ~body ()) in *)
-(*       Result.Ok resp *)
-(*     | _ -> *)
-(*       Result.Error "Expected InitializeRequest" *)
-
-(* end *)
-
-
-(* type ('request, 'body, 'response) flow = 'request -> 'body -> ('response, ErrorResponse_body.t ErrorResponseMessage.t) Result.t *)
-(* let cancel : (CancelArguments.t CancelRequestMessage.t, unit, unit CancelResponseMessage.t) flow *)
-(*   = fun req body -> *)
-(*     let module S = Sequencing (CancelRequestMessage) in *)
-(*     let {S.seq; request_seq} = S.correct_sequence req in *)
-(*     let resp = CancelResponseMessage.make ~seq ~request_seq ~success:true ~body () in *)
-(*     Result.Ok resp *)
-
-(* let init : (InitializeRequestArguments.t InitializeRequestMessage.t, Capabilities.t, Capabilities.t InitializeResponseMessage.t) flow *)
-(*   = fun req body -> *)
-(*     let module S = Sequencing (InitializeRequestMessage) in *)
-(*     let {S.seq; request_seq} = S.correct_sequence req in *)
-(*     let resp = InitializeResponseMessage.make ~seq ~request_seq ~success:true ~body () in *)
-(*     Result.Ok resp *)
-
-(* let config : (ConfigurationDoneArguments.t ConfigurationDoneRequestMessage.t, Dap_t.EmptyObject.t, Dap_t.EmptyObject.t ConfigurationDoneResponseMessage.t) flow *)
-(*   = fun req body -> *)
-(*     let module S = Sequencing (ConfigurationDoneRequestMessage) in *)
-(*     let {S.seq; request_seq} = S.correct_sequence req in *)
-(*     let resp = ConfigurationDoneResponseMessage.make ~seq ~request_seq ~success:true ~body () in *)
-(*     Result.Ok resp *)
-
-open Dap_t
-
-module Flow (Request:REQUEST) (Response:RESPONSE) = struct
-
+  open Dap_t
   open Dap_message
 
-  type ('command, 'args, 'presence_args, 'body, 'presence_body) t = {
-    request: ('command, 'args, 'presence_args) Request.t;
-    response: ('command, 'body, 'presence_body) Response.t;
-    events: event list;
-    make_error: seq:int -> request_seq:int -> success:bool -> ?message:string -> body:ErrorResponse_body.t -> unit -> response ;
-  }
+  module JS = Data_encoding.Json
+  module SReq = Sequencing(RequestMessage)
+  module SResp = Sequencing(ResponseMessage)
+  module SEv = Sequencing(EventMessage)
 
-  let make ?(events=[]) request response =
-    let make_error = fun ~seq ~request_seq ~success ?message ~body () ->
-      let cnl = Dap_command.cancel in
-      let _b = EmptyObject.make () in
-      let c = ResponseMessage.make_opt ~seq ~request_seq ~success ~command:cnl ?message ~body:_b () in
-      let _ = CancelResponse c in
-      let command = Dap_command.error in
-      let e = ResponseMessage.make ~seq ~request_seq ~success ~command ?message ~body () in
-      ErrorResponse e
-    in
-    {request; response; events; make_error}
+  type launch_mode = [ `Launch | `Attach | `AttachForSuspendedLaunch ]
+  type config = {launch_mode: launch_mode}
+  type ret = (response * event list, ErrorResponse_body.t) Result.t
 
+  let request_response ~config = function
+    | CancelRequest req ->
+      let resp =
+        let sequencer = SReq.next_sequence req in
+        let request_seq = SReq.request_seq sequencer in
+        let seq = SReq.seq sequencer in
+        let command = RequestMessage.command req in
+        let body = EmptyObject.make () in
+        ResponseMessage.make_opt ~seq ~request_seq ~success:true ~command ~body ()
+      in
+      let ret = CancelResponse resp, [] in
+      Result.ok ret
+
+(*
+Initialization
+
+The Debug Adapter Protocol defines many features and this number is still growing, albeit slowly. However, the protocol is still at its first version because it was an explicit design goal to support new feature in a completely backward compatible way. Making this possible without version numbers requires that every new feature gets a corresponding flag that lets a development tool know whether a debug adapter supports the feature or not. The absence of the flag always means that the feature is not supported.
+
+A single feature and its corresponding flag is called a “capability” in the Debug Adapter Protocol. The open-ended set of all features flags is called DAP’s “capabilities.”
+
+When starting a debug session, the development tool sends an initialize request to the adapter in order to exchange capabilities between the development tool and the debug adapter.
+
+The development tool capabilities are provided in the InitializeRequestArguments structure of the initialize request and typically start with the prefix supports. Other pieces of information passed from the tool to the debug adapter are:
+
+    the name of the development tool,
+    the format of file paths, native or uri,
+    whether line and column values are 0 or 1 based,
+    the locale used by the development tool. A debug adapter is expected to return error messages that honor this locale.
+
+The debug adapter returns the supported capabilities in the InitializeResponse via the Capabilities type. It is not necessary to return an explicit false for unsupported capabilities.
+ *)
+    | InitializeRequest req ->
+      let resp =
+        let sequencer = SReq.next_sequence req in
+        let request_seq = SReq.request_seq sequencer in
+        let seq = SReq.seq sequencer in
+        let command = RequestMessage.command req in
+        (* TODO hardcode capabilities or pull in from config *)
+        let body = Capabilities.make () in
+        ResponseMessage.make_opt ~seq ~request_seq ~success:true ~command ~body ()
+      in
+      let ev =
+        let seq = SResp.(next_sequence resp |> seq) in
+        let event = Dap_event.initialized in
+        let body = EmptyObject.make () in
+        EventMessage.make_opt ~seq ~event ~body ()
+      in
+      let ret = InitializeResponse resp, [InitializedEvent ev] in
+      Result.ok ret
+
+    (* TODO breakpoints stuff - here *)
+
+    | ConfigurationDoneRequest req ->
+      let resp =
+        let sequencer = SReq.next_sequence req in
+        let request_seq = SReq.request_seq sequencer in
+        let seq = SReq.seq sequencer in
+        let command = RequestMessage.command req in
+        let body = EmptyObject.make () in
+        ResponseMessage.make_opt ~seq ~request_seq ~success:true ~command ~body ()
+      in
+      let ret = ConfigurationDoneResponse resp, [] in
+      Result.ok ret
+
+    (*
+Launching and attaching
+
+After the debug adapter has been initialized, it is ready to accept requests for starting debugging. Two requests exist for this:
+
+    launch request: the debug adapter launches the program (“debuggee”) in debug mode and then starts to communicate with it. Since the debug adapter is responsible for launching the debuggee, it should provide a mechanism for the end user to configure the debuggee. For example, passing arguments or specifying a working directory.
+        Debug adapters are free to launch the debuggee however they choose. Typically the debuggee is launched as a child process and its output channels are connected to a client’s debug console via output events. However, this has certain limitations, such as not being able to write to the terminal device directly and not being able to accept standard input. For those cases, launching the debuggee in a terminal is preferable. A debug adapter can use the the runInTerminal request to ask the client to launch the debuggee in a terminal that is integrated into the client or in a terminal that runs outside of the client (but still configured and managed from the client).
+    attach request: the debug adapter connects to an already running program. Here the end user is responsible for launching and terminating the program.
+
+Since arguments for both requests are highly dependent on a specific debugger and debug adapter implementation, the Debug Adapter Protocol does not specify any arguments for these requests. Instead, the development tool is expected to get information about debugger specific arguments from elsewhere (e.g. contributed by some plugin or extension mechanism) and to build a UI and validation mechanism on top of that.
+  *)
+    | LaunchRequest req when config.launch_mode = `Launch ->
+      let resp =
+        let sequencer = SReq.next_sequence req in
+        let request_seq = SReq.request_seq sequencer in
+        let seq = SReq.seq sequencer in
+        let command = RequestMessage.command req in
+        let body = EmptyObject.make () in
+        ResponseMessage.make_opt ~seq ~request_seq ~success:true ~command ~body ()
+      in
+      let ev =
+        let seq = SResp.(next_sequence resp |> seq) in
+        let event = Dap_event.process in
+        let startMethod = ProcessEvent_body_startMethod.Launch in
+        let body = ProcessEvent_body.make ~name:"TODO PROCESS EVENT NAME e.g. test.tz" ~startMethod () in
+        EventMessage.make ~seq ~event ~body ()
+      in
+
+      let ret = LaunchResponse resp, [ProcessEvent ev] in
+      Result.ok ret
+
+    | LaunchRequest _req when config.launch_mode = `Attach ->
+      Logs.err (fun m -> m "wrong launch mode - config is set to Attach but got a Launch request message");
+      let error = Message.make ~id:0 ~format:"wrong launch mode - config is set to Attach but got a Launch request message" () in
+      let error = ErrorResponse_body.make ~error () in
+      Result.error error
+
+    | AttachRequest req when config.launch_mode = `Attach ->
+      let resp =
+        let sequencer = SReq.next_sequence req in
+        let request_seq = SReq.request_seq sequencer in
+        let seq = SReq.seq sequencer in
+        let command = RequestMessage.command req in
+        let body = EmptyObject.make () in
+        ResponseMessage.make_opt ~seq ~request_seq ~success:true ~command ~body ()
+      in
+      let ev =
+        let seq = SResp.(next_sequence resp |> seq) in
+        let event = Dap_event.process in
+        let startMethod = ProcessEvent_body_startMethod.Attach in
+        let body = ProcessEvent_body.make ~name:"TODO PROCESS EVENT NAME e.g. test.tz" ~startMethod () in
+        EventMessage.make ~seq ~event ~body ()
+      in
+
+      let ret = AttachResponse resp, [ProcessEvent ev] in
+      Result.ok ret
+
+    | AttachRequest _req when config.launch_mode = `Launch ->
+      Logs.err (fun m -> m "wrong launch mode - config is set to Launch but got a Attach request message");
+      let error = Message.make ~id:0 ~format:"wrong launch mode - config is set to Launch but got a Attach request message" () in
+      let error = ErrorResponse_body.make ~error () in
+      Result.error error
+
+    | RestartRequest req ->
+      let resp =
+        let sequencer = SReq.next_sequence req in
+        let request_seq = SReq.request_seq sequencer in
+        let seq = SReq.seq sequencer in
+        let command = RequestMessage.command req in
+        let body = EmptyObject.make () in
+        ResponseMessage.make_opt ~seq ~request_seq ~success:true ~command ~body ()
+      in
+      (* TODO any events to raise? *)
+      let ret = RestartResponse resp, [] in
+      Result.ok ret
+
+
+(*
+ Debug session end
+
+When the development tool ends a debug session, the sequence of events is slightly different based on whether the session has been initially “launched” or “attached”:
+
+    Debuggee launched: if a debug adapter supports the terminate request, the development tool uses it to terminate the debuggee gracefully, i.e. it gives the debuggee a chance to cleanup everything before terminating. If the debuggee does not terminate but continues to run (or hits a breakpoint), the debug session will continue, but if the development tool tries again to terminate the debuggee, it will then use the disconnect request to end the debug session unconditionally. The disconnect request is expected to terminate the debuggee (and any child processes) forcefully.
+    Debuggee attached: If the debuggee has been “attached” initially, the development tool issues a disconnect request. This should detach the debugger from the debuggee but will allow it to continue.
+
+In all situations where a debug adapter wants to end the debug session, a terminated event must be fired.
+
+If the debuggee has ended (and the debug adapter is able to detect this), an optional exited event can be issued to return the exit code to the development tool.Debug session end
+
+When the development tool ends a debug session, the sequence of events is slightly different based on whether the session has been initially “launched” or “attached”:
+
+    Debuggee launched: if a debug adapter supports the terminate request, the development tool uses it to terminate the debuggee gracefully, i.e. it gives the debuggee a chance to cleanup everything before terminating. If the debuggee does not terminate but continues to run (or hits a breakpoint), the debug session will continue, but if the development tool tries again to terminate the debuggee, it will then use the disconnect request to end the debug session unconditionally. The disconnect request is expected to terminate the debuggee (and any child processes) forcefully.
+    Debuggee attached: If the debuggee has been “attached” initially, the development tool issues a disconnect request. This should detach the debugger from the debuggee but will allow it to continue.
+
+In all situations where a debug adapter wants to end the debug session, a terminated event must be fired.
+
+If the debuggee has ended (and the debug adapter is able to detect this), an optional exited event can be issued to return the exit code to the development tool.
+  *)
+
+    | DisconnectRequest req ->
+      let resp =
+        let sequencer = SReq.next_sequence req in
+        let request_seq = SReq.request_seq sequencer in
+        let seq = SReq.seq sequencer in
+        let command = RequestMessage.command req in
+        let body = EmptyObject.make () in
+        ResponseMessage.make_opt ~seq ~request_seq ~success:true ~command ~body ()
+      in
+      (* TODO any events to raise? *)
+      let ret = DisconnectResponse resp, [] in
+      Result.ok ret
+
+    | TerminateRequest req ->
+      let resp =
+        let sequencer = SReq.next_sequence req in
+        let request_seq = SReq.request_seq sequencer in
+        let seq = SReq.seq sequencer in
+        let command = RequestMessage.command req in
+        let body = EmptyObject.make () in
+        ResponseMessage.make_opt ~seq ~request_seq ~success:true ~command ~body ()
+      in
+      (* TODO any events to raise? *)
+      let ret = TerminateResponse resp, [] in
+      Result.ok ret
+
+    | _ -> failwith "TODO"
+
+
+
+
+
+  (* let _HEADER_FIELD = "Content-Length: " *)
+  (* let _HEADER_TOKEN = "\r\n\r\n" *)
+
+  (* let _replace input output = *)
+  (*   Str.global_replace (Str.regexp_string input) output *)
+
+  (* let wrap_header js = *)
+  (*   let s = js *)
+  (*           |> JS.to_string *)
+  (*           |> _replace "\n" "" *)
+  (*   in *)
+  (*   let n = String.length s in *)
+  (*   Printf.sprintf "%s%d%s%s" _HEADER_FIELD n _HEADER_TOKEN s *)
+
+  (* let destruct_request t msg = *)
+  (*     match JS.from_string msg with *)
+  (*     | Ok js -> ( *)
+  (*         try *)
+  (*           Ok (JS.destruct Request.enc t.request js) *)
+  (*         with _ as err -> *)
+  (*           Logs.err (fun m -> m "Cannot parse json '%s' as request: '%s'" msg @@ Printexc.to_string err); *)
+  (*           Error (Printexc.to_string err) *)
+  (*       ) *)
+  (*     | Error err -> *)
+  (*       Logs.err (fun m -> m "Cannot parse json '%s': '%s'" msg err); *)
+  (*       (\* TODO should return an error response *\) *)
+  (*       Error err *)
+
+
+  (* let construct_response t response = *)
+  (*   let r = Response.incr response in *)
+  (*   JS.construct t.response r |> wrap_header *)
 end
