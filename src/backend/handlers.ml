ade3edaeb7b18e4c62d5d6c7af8eb7514176a7f8
wyn <simon.parry@karoshibee.com>
2022-10-24 09:12:04 +0100

    handlers for init/terminate

--- src/backend/handlers.ml
+++ src/backend/handlers.ml
@@ -1,5 +1,5 @@
 open Handler_t
-
+module Dap_commands = Dapper.Dap_commands
 
 module type MAKE_HANDLER = sig
   type input
@@ -36,3 +36,45 @@ module Handler (H:HANDLER) : (MAKE_HANDLER with type input := H.input and type o
 
 
 end
+
+type t = (string, (module HANDLER)) Hashtbl.t
+
+let make : t = [
+  "Cancel", (module Cancel);
+  "Initialize", (module Initialize);
+  "ConfigurationDone", (module Configuration);
+  "Launch", (module Launch);
+  "Attach", (module Attach);
+  "Restart", (module Restart);
+  "Disconnect", (module Disconnect);
+  "Terminate", (module Terminate);
+] |> List.map (fun (name, m) ->
+    let module M = (val m : HANDLER) in
+    let key = String.uncapitalize_ascii name in
+    (key, m)
+  )
+  |> List.to_seq
+  |> Hashtbl.of_seq
+
+
+let handle_exn t config message =
+  let aux command =
+    Hashtbl.find_opt t command
+      |> fun h -> Option.bind h (fun h ->
+        let module H = Handler (val h : HANDLER) in
+        let h = H.make in
+        try
+          Option.some @@ H.handle h ~config message
+        with _ ->
+          None
+      )
+  in
+  ["cancel";
+   "initialize";
+   "configurationDone";
+   "launch";
+   "attach";
+   "restart";
+   "disconnect";
+   "terminate";
+  ] |> List.find_map aux |> Option.get
