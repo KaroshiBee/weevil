f32f37c13161349be25f8f8b482d17289b00b2b1
wyn <simon.parry@karoshibee.com>
2022-10-27 12:25:40 +0100

    added test for raise_event flow

--- src/dapper/dap_flow_tests.ml
+++ src/dapper/dap_flow_tests.ml
@@ -11,11 +11,14 @@ let%expect_test "Check sequencing request/response/event" =
       () in
   let enc = RequestMessage.enc_opt Dap_commands.cancel CancelArguments.enc in
   let s = Js.construct enc cancel |> Js.to_string in
+
+  (* Check json encoding of a request *)
   Printf.printf "%s" s;
   [%expect {|
     { "seq": 10, "type": "request", "command": "cancel",
       "arguments": { "requestId": 1 } } |}];
 
+  (* check request/response state change *)
   let f = fun _ ->
     (* NOTE could also get the command off input *)
     let command = Dap_commands.cancel in
@@ -48,6 +51,46 @@ let%expect_test "Check sequencing request/response/event" =
     { "seq": 11, "type": "response", "request_seq": 10, "success": true,
       "command": "cancel", "body": {} } |}];
 
+  (* check request/error state change *)
+  let f = fun _ ->
+    let e = "something went wrong" in
+    let id = Hashtbl.hash e in
+    let variables = `O [("error", `String e)] in
+    let error = Message.make ~id ~format:"{error}" ~variables () in
+    let body = ErrorResponse_body.make ~error () in
+    let command = Dap_commands.error in
+    ErrorResponse (
+      ResponseMessage.make
+        ~seq:0
+        ~request_seq:0
+        ~success:false
+        ~command
+        ~body
+        ()
+    ) |> Dap_flow.from_response
+  in
+  let v = Dap_flow.(
+      from_request @@ CancelRequest cancel
+      |> fun v -> raise_error v f
+    )
+  in
+  let s =
+    match Dap_flow.to_result v with
+    | Result.Ok (ErrorResponse resp) ->
+      let enc = ResponseMessage.enc Dap_commands.error ErrorResponse_body.enc in
+      Js.construct enc resp |> Js.to_string
+    | _ -> assert false
+  in
+  Printf.printf "%s" s;
+  [%expect {|
+    { "seq": 11, "type": "response", "request_seq": 10, "success": false,
+      "command": "error",
+      "body":
+        { "error":
+            { "id": 1022476669, "format": "{error}",
+              "variables": { "error": "something went wrong" } } } } |}];
+
+  (* check response/event state change *)
   let f = fun _ ->
     let event = Dap_events.terminated in
     let body  = None in
@@ -71,6 +114,7 @@ let%expect_test "Check sequencing request/response/event" =
   Printf.printf "%s" s;
   [%expect {| { "seq": 12, "type": "event", "event": "terminated" } |}];
 
+  (* check event/event state change *)
   let f = fun _ ->
     let event = Dap_events.exited in
     let body = ExitedEvent_body.make ~exitCode:0 () in
