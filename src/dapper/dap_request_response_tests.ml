e7cb63bdd635bbad8aa8f324002e0b59e588b807
wyn <simon.parry@karoshibee.com>
2022-11-02 14:30:15 +0000

    couple more error tests

--- src/dapper/dap_request_response_tests.ml
+++ src/dapper/dap_request_response_tests.ml
@@ -104,6 +104,7 @@ let%expect_test "Check sequencing request/response" =
   let req_attach = attachRequest @@ RequestMessage.make ~seq:100 ~command:Dap_commands.attach ~arguments:(AttachRequestArguments.make ()) () in
   let enc_error = ResponseMessage.enc Dap_commands.error ErrorResponse_body.enc in
 
+  (* should error with correct seq numbers if given the wrong request type *)
   let s =
     handler req_attach |> Result.map_error (fun resp ->
         Js.construct enc_error resp |> Js.to_string
@@ -116,4 +117,27 @@ let%expect_test "Check sequencing request/response" =
       "body":
         { "error":
             { "id": 342703193, "format": "{error}",
-              "variables": { "error": "wrong request: expected LaunchRequest" } } } } |}]
+              "variables": { "error": "wrong request: expected LaunchRequest" } } } } |}];
+
+  (* should also have the correct seq numbers if error happens even with correct input *)
+  let handler_err =
+    let l = Launch.make (fun _req ->
+        default_response_error "testing error"
+        |> Result.error
+      )
+    in
+    Launch.handle l
+  in
+  let s =
+    handler_err req_launch |> Result.map_error (fun resp ->
+        Js.construct enc_error resp |> Js.to_string
+      ) |> Result.get_error
+  in
+  Printf.printf "%s" s;
+  [%expect {|
+    { "seq": 11, "type": "response", "request_seq": 10, "success": false,
+      "command": "error",
+      "body":
+        { "error":
+            { "id": 400237674, "format": "{error}",
+              "variables": { "error": "testing error" } } } } |}]
