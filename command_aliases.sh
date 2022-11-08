a93d0dd7b5777d82ba12c4f40a242bb557b6947b
wyn <simon.parry@karoshibee.com>
2022-11-08 12:53:35 +0000

    pimped up the alias file

--- command_aliases.sh
+++ command_aliases.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env sh
 
+echo ""
+echo "** These aliases are just for dev/test purposes **"
+echo "** they will only work from the weevil project root directory **"
+echo ""
+
 alias weevil='dune exec -- ./src/main.exe'
-alias weevil-messages='dune exec -- ./src/main.exe dap-gen messages ./schema/debugAdapterProtocol-1.56.X.json $HOME/dap_message'
-alias weevil-commands='dune exec -- ./src/main.exe dap-gen commands ./schema/debugAdapterProtocol-1.56.X.json $HOME/dap_commands'
-alias weevil-events='dune exec -- ./src/main.exe dap-gen events ./schema/debugAdapterProtocol-1.56.X.json $HOME/dap_events'
+echo "alias weevil"
+
+alias weevil-messages='dune exec -- ./src/main.exe dap-gen messages ./schema/debugAdapterProtocol-1.56.X.json $HOME/dap_message && echo "output: $HOME/dap_message.ml"'
+echo "alias weevil-messages"
+
+alias weevil-commands='dune exec -- ./src/main.exe dap-gen commands ./schema/debugAdapterProtocol-1.56.X.json $HOME/dap_commands && echo "output: $HOME/dap_commands.ml[i]"'
+echo "alias weevil-commands"
+
+alias weevil-events='dune exec -- ./src/main.exe dap-gen events ./schema/debugAdapterProtocol-1.56.X.json $HOME/dap_events && echo "output: $HOME/dap_events.ml[i]"'
+echo "alias weevil-events";
