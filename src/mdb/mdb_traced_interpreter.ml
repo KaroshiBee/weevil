0446d364932dd275c4b91ed7ed05d7267ef1114e
wyn <simon.parry@karoshibee.com>
2022-11-29 14:57:57 +0000

    updated to new syntax

--- src/mdb/mdb_traced_interpreter.ml
+++ src/mdb/mdb_traced_interpreter.ml
@@ -18,28 +18,31 @@ module T (Cfg : Mdb_types.INTERPRETER_CFG) = struct
         * ('a, 's) Script_typed_ir.stack_ty
         -> log_element
 
-  let unparse_stack ctxt (stack, stack_ty) =
+  let unparse_stack ~oc:_ ctxt (stack, stack_ty) =
+    let open Lwt_result_syntax in
     (* We drop the gas limit as this function is only used for debugging/errors. *)
     let ctxt = Gas.set_unlimited ctxt in
     let rec unparse_stack :
       type a s.
       (a, s) Script_typed_ir.stack_ty * (a * s) ->
-      Script.expr list tzresult Lwt.t = function
+      (Script.expr * string option * bool) list tzresult Lwt.t = function
       | Bot_t, (EmptyCell, EmptyCell) -> return_nil
       | Item_t (ty, rest_ty), (v, rest) ->
-        Script_ir_translator.unparse_data
-          ctxt
-          Cfg.unparsing_mode
-          ty
-          v
-        >>=? fun (data, _ctxt) ->
-        unparse_stack (rest_ty, rest) >|=? fun rest ->
+        let* (data, _ctxt) = Script_ir_translator.unparse_data ctxt Cfg.unparsing_mode ty v in
+        let+ rest = unparse_stack (rest_ty, rest) in
         let data = Environment.Micheline.strip_locations data in
-        data :: rest
+        (data, None, false) :: rest
     in
     unparse_stack (stack_ty, stack)
 
-  let trace_logger ~in_channel:ic ~out_channel:_ () : Script_typed_ir.logger =
+  let unparse_log ~oc (Log (ctxt, loc, stack, stack_ty)) =
+    let open Lwt_result_syntax in
+    trace
+      Plugin.Plugin_errors.Cannot_serialize_log
+      (let* stack = unparse_stack ~oc ctxt (stack, stack_ty) in
+       return (loc, Gas.level ctxt, stack))
+
+  let trace_logger ~in_channel:ic ~out_channel:oc () : Script_typed_ir.logger =
 
     let log : log_element list ref = ref [] in
 
@@ -50,23 +53,49 @@ module T (Cfg : Mdb_types.INTERPRETER_CFG) = struct
     let log_entry _ _ctxt loc _sty _stack =
       Logs.info (fun m -> m "log_entry @ location %d" loc);
       (* block waiting for a \n on in_channel *)
-      let _msg = input_line ic in
-      ()
+      let msg = input_line ic in
+      Logs.info (fun m -> m "got msg '%s'" msg)
     in
     let log_exit _ ctxt loc sty stack =
       Logs.info (fun m -> m "log_exit @ location %d" loc);
-      log := Log (ctxt, loc, stack, sty) :: !log
+      let l = Log (ctxt, loc, stack, sty) in
+      (* TODO can we serialise l and send that? then unparse on main thread in order*)
+      let _ = Lwt.ignore_result @@ (
+          let open Lwt_result_syntax in
+          let*! is_sent = (
+            let* (_loc, gas, expr) = unparse_log ~oc l in
+            let wrec = Mdb_model.Weevil_record.make loc gas expr in
+            let wrec_js = Mdb_model.Weevil_record.to_weevil_json wrec in
+            let js = Data_encoding.Json.(
+                construct Mdb_model.Weevil_json.enc wrec_js
+                |> to_string
+                |> Dapper.Dap.Header.wrap
+              ) in
+            return @@ Printf.(fprintf oc "%s\n" js; flush oc)
+          )
+          in
+          match is_sent with
+          | Ok () ->
+            Lwt.return @@ Logs.info (fun m -> m "sent weevil record")
+          | Error errs ->
+            Lwt.return @@ Logs.err (fun m -> m "%s" @@ Data_encoding.Json.(construct trace_encoding errs |> to_string))
+        )
+      in
+      log := l :: !log
     in
     let log_control _ =
       Logs.info (fun m -> m "log_control");
     in
     let get_log () =
+      let open Lwt_result_syntax in
       Environment.List.map_es
         (fun (Log (ctxt, loc, stack, stack_ty)) ->
            trace
              Plugin.Plugin_errors.Cannot_serialize_log
-             (unparse_stack ctxt (stack, stack_ty))
-           >>=? fun stack -> return (loc, Gas.level ctxt, stack))
+             (let* stack = unparse_stack ~oc ctxt (stack, stack_ty) in
+              let stack = Environment.List.map (fun (expr, _, _) -> expr) stack in
+              return (loc, Gas.level ctxt, stack))
+        )
         !log
       >>=? fun res -> return (Some (List.rev res))
     in
