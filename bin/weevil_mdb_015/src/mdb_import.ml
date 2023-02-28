f5aa0a989423085fb3f85e85200ead686258dd1f
wyn <simon.parry@karoshibee.com>
2023-02-28 14:35:51 +0000

    added proto 015 stepper

---
+++ bin/weevil_mdb_015/src/mdb_import.ml
@@ -0,0 +1,27 @@
+module type PROT   = module type of Tezos_protocol_015_PtLimaPt.Protocol
+module type CTXT   = module type of Tezos_protocol_015_PtLimaPt.Protocol.Alpha_context
+module type ENV    = module type of Tezos_protocol_015_PtLimaPt.Environment
+module type ERR    = module type of Tezos_protocol_015_PtLimaPt.Environment.Error_monad
+module type PLUGIN = module type of Tezos_protocol_plugin_015_PtLimaPt
+module type CLIENT = module type of Tezos_client_015_PtLimaPt
+
+module MakeTezos (P:PROT) (Env:ENV) (C:CLIENT) (Plugin:PLUGIN) = struct
+  module Prot = P
+  module Env = Env
+  module Ctxt = P.Alpha_context
+  module Err = Env.Error_monad
+  module Plugin = Plugin
+  module Client = C
+  module PP = C.Michelson_v1_printer
+  module Parser = C.Michelson_v1_parser
+end
+
+module Tez = MakeTezos
+    (Tezos_protocol_015_PtLimaPt.Protocol)
+    (Tezos_protocol_015_PtLimaPt.Environment)
+    (Tezos_client_015_PtLimaPt)
+    (Tezos_protocol_plugin_015_PtLimaPt)
+
+module type TEZ = module type of Tez
+
+let protocol_str = "PtLimaPt"
---
+++ bin/weevil_mdb_015/src/mdb_traced_interpreter.ml
@@ -0,0 +1,181 @@
+module Log_records = Mdb_log_records
+open Mdb_import.Tez
+
+type t = Prot.Script_typed_ir.logger
+
+type cfg_t =
+  | Log :
+      Ctxt.context
+      * Tezos_micheline.Micheline_parser.location
+      * ('a * 's)
+      * ('a, 's) Prot.Script_typed_ir.stack_ty
+      -> cfg_t
+
+let make_log ctxt loc stack stack_ty =
+  Log (ctxt, loc, stack, stack_ty)
+
+let unparsing_mode = Prot.Script_ir_unparser.Readable
+
+let unparse_stack = function
+  | Log (ctxt_in, _loc, stack, stack_ty) ->
+    let open Lwt_result_syntax in
+    (* We drop the gas limit as this function is only used for debugging/errors. *)
+    let ctxt = Ctxt.Gas.set_unlimited ctxt_in in
+    let rec _unparse_stack :
+      type a s.
+      (a, s) Prot.Script_typed_ir.stack_ty * (a * s) ->
+      (Ctxt.Script.expr * string option * bool) list Err.tzresult Lwt.t = function
+      | Bot_t, (EmptyCell, EmptyCell) -> return_nil
+      | Item_t (ty, rest_ty), (v, rest) ->
+        let* (data, _ctxt) =
+          Prot.Script_ir_translator.unparse_data ctxt unparsing_mode ty v
+        in
+        let+ rest = _unparse_stack (rest_ty, rest) in
+        (data, None, false) :: rest
+    in
+    _unparse_stack (stack_ty, stack)
+
+let get_loc = function
+  | Log (_, loc, _, _) -> loc
+
+let get_gas = function
+  | Log (ctxt, _, _, _) -> Ctxt.Gas.level ctxt
+
+let log_element_to_json log_element =
+  let open Lwt_result_syntax in
+  let module List = Env.List in
+  let* (loc, gas, exprs) =
+    Err.trace
+      Plugin.Plugin_errors.Cannot_serialize_log
+      (let* stack = unparse_stack log_element in
+       let stack = List.map (fun (expr, _, _) -> expr) stack in
+       let loc = get_loc log_element in
+       let gas = get_gas log_element in
+       return (loc, gas, stack))
+  in
+  return (loc, gas, exprs)
+
+let log_element_to_out log_element out_channel =
+  (* unparse the log_element and wait for the data *)
+  let to_out : (Tezos_micheline.Micheline_parser.location * Ctxt.Gas.t * 'exprs) option ref = ref None in
+  let lck = Lwt_mutex.create () in
+  let () =
+    Lwt.async (fun () ->
+        let open Lwt_syntax in
+        let* () = Lwt_mutex.with_lock lck (fun () ->
+            let* () = Logs_lwt.debug (fun m -> m "async log element to json") in
+            let* res = log_element_to_json log_element in
+            match res with
+            | Ok t ->
+              let* () = Logs_lwt.debug (fun m -> m "async log element to json, got data, writing to to_out") in
+              let () = to_out := Some t in
+              Lwt.return_unit
+            | Error err ->
+              let s = Format.asprintf "%a" Err.pp_trace err in
+              let* () = Logs_lwt.err (fun m -> m "async log element to json, got error %s" s) in
+              Lwt.return_unit
+          )
+        in
+        Lwt.return_unit
+      )
+  in
+
+  let pp_exprs =
+    let pp expr =
+      (* TODO better handling of mich expressions/tickets *)
+      Format.asprintf "%a" PP.print_expr expr
+    in
+    List.map pp
+  in
+
+  let pp_gas gas =
+    Format.asprintf "%a" Ctxt.Gas.pp gas
+  in
+
+  let rec aux () =
+    match Lwt_mutex.is_locked lck, !to_out with
+    | false, Some (location, gas, exprs) ->
+      let () = Logs.debug (fun m -> m "pp gas") in
+      let gas = pp_gas gas in
+      let () = Logs.debug (fun m -> m "pp stack") in
+      let stack = pp_exprs exprs in
+      let () = Logs.debug (fun m -> m "making weevil js record") in
+      let wrec_js = Mdb_model.make ~location ~gas ~stack () in
+      let js = Data_encoding.Json.(
+          construct Mdb_model.enc wrec_js
+          |> to_string
+          |> Dapper.Dap.Header.wrap
+        ) in
+      let () = Logs.debug (fun m -> m "writing back child process -> mdb backend") in
+      let () = Printf.(fprintf out_channel "%s" js; flush out_channel) in
+      to_out := None
+
+    | true, _ | _, None ->
+      let () = Logs.debug (fun m -> m "waiting for to_out") in
+      Unix.sleepf 0.1;
+      aux ()
+
+  in
+  aux ()
+
+let make ~in_channel ~out_channel file_locations =
+
+  let log_interp _ ctxt loc sty stack =
+    Logs.debug (fun m -> m "log_interp @ location %d" loc);
+    let file_loc = Mdb_michelson.File_locations.get file_locations loc in
+
+    let () = file_loc
+             |> Option.map (fun floc ->
+                 let log_element = make_log ctxt floc stack sty in
+                 log_element_to_out log_element out_channel
+               )
+             |> Option.value ~default:()
+    in
+
+    (* block waiting for a \n on in_channel *)
+    let msg = input_line in_channel in
+    Logs.debug (fun m -> m "got msg '%s'" msg)
+
+  in
+  let log_entry _ _ctxt loc _sty _stack =
+    Logs.debug (fun m -> m "log_entry @ location %d" loc);
+  in
+  let log_exit _ ctxt loc sty stack =
+    Logs.debug (fun m -> m "log_exit @ location %d" loc);
+    let file_loc = Mdb_michelson.File_locations.get file_locations loc in
+
+    let () = file_loc
+             |> Option.map (fun floc ->
+                 let log_element = make_log ctxt floc stack sty in
+                 log_element_to_out log_element out_channel
+               )
+             |> Option.value ~default:()
+    in
+
+    (* block waiting for a \n on in_channel *)
+    let msg = input_line in_channel in
+    Logs.debug (fun m -> m "got msg '%s'" msg)
+
+  in
+  let log_control _ =
+    (* i think this is for lambdas etc *)
+    Logs.debug (fun m -> m "log_control");
+  in
+  let get_log () =
+    (* NOTE we dont need to use get_log anymore *)
+    let open Lwt_result_syntax in
+    return @@ Some []
+  in
+  Prot.Script_typed_ir.{log_exit; log_entry; log_interp; get_log; log_control}
+
+let execute ctxt step_constants ~script ~entrypoint ~parameter ~interp =
+  Prot.Script_interpreter.execute
+    ~logger:interp
+    ~cached_script:None
+    ctxt
+    unparsing_mode
+    step_constants
+    ~script
+    ~entrypoint
+    ~parameter
+    ~internal:true
