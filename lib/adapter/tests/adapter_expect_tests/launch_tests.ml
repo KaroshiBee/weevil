bac041e65b28de573e6193470890efff65bacfe0
wyn <simon.parry@karoshibee.com>
2023-04-19 11:36:33 +0100

    updated tests,
    TODO need better way of specifying internal cmdlines

--- lib/adapter/tests/adapter_expect_tests/launch_tests.ml
+++ lib/adapter/tests/adapter_expect_tests/launch_tests.ml
@@ -99,9 +99,9 @@ let%expect_test "Check sequencing etc for launch" =
               Printf.printf "%s" s;
               let%lwt () =
                 [%expect {|
-                  Content-Length: 173
+                  Content-Length: 181
                   
-                  { "event":    { "cmd":        "dune exec -- weevil stepper -v -v --headless --storage 'Unit' --parameter 'Unit' --entrypoint 'default' data/multiply_2_x_25_equals_50.tz" } } |}]
+                  { "event":    { "cmd":        "dune exec -- weevil_mdb_016 stepper -v -v --headless --storage 'Unit' --parameter 'Unit' --entrypoint 'default' data/multiply_2_x_25_equals_50.tz" } } |}]
               in
               Lwt.return_unit
             )
